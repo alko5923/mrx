b912e3d628442ce4bae857b6e3e35b15e5a620d6
Aljaz Kovac <aljaz.kovac.5923@student.uu.se>
2020-12-02 12:00:45 +0100

    started working on main function, modified .gitignore file

--- src/simulate/Hunt.java
+++ src/simulate/Hunt.java
@@ -1,8 +1,41 @@
 package simulate;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Hunt {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Mr X, the codebreaker for the game Scotland Yard!");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			
+			
+			System.out.println("Please enter the number of players: ");
+			
+			try {
+				int numberOfPlayers = sc.nextInt();
+				System.out.println("The number of players is" + " " + numberOfPlayers);
+			}
+			
+			catch(IllegalStateException e) {
+				System.out.println(e.getMessage());
+			}
+			catch(NoSuchElementException e) {
+				System.out.println(e.getMessage());
+			}
+			
+			finally {
+				sc.close();
+			}
+			break;
+			
+			
+		}
+		
+		
 
 	}
 
