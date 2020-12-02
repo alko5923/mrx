4b9c66c5f387de52d4ed79cacc6f981d8e17905a
Aljaz Kovac <aljaz.kovac.5923@student.uu.se>
2020-12-02 16:58:56 +0100

    worked on Connection class and other small changes

--- src/map/Connection.java
+++ src/map/Connection.java
@@ -1,11 +1,25 @@
 package map;
 
 public class Connection {
-	private String[] type; 
+	private Location start;
+	private Location end;
+	private String type; 
 	
-	public Connection(String... type) {
-		// TODO
+	public Connection(Location start, Location end, String type) {
+		this.start = start;
+		this.end = end;
+		this.type = type;
 	}
 	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Connection ");
+		sb.append("from ");
+		sb.append(this.start);
+		sb.append(" to ");
+		sb.append(this.end);
+		sb.append(" and vice versa.");
+		return sb.toString();
+	}
 	
 }
